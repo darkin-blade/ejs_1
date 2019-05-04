83aa09a665bb2f161307ed1d27609f7bcae263c9
Lynx <1197225628@qq.com>
2019-05-04 16:19:53 +0800

    半页

--- source/js/change.js
+++ source/js/change.js
@@ -14,11 +14,43 @@ function my_load()
 
 function change_width() // 自动调整元素宽度
 {
-  var temp = (window.innerWidth - 600) / 1920 * 10;
-  if (temp <= 0) temp = 0;
-  $(".mid_container").css("margin-right", 2 + temp + "%");
-  $(".mid_container").css("margin-left", 2 + temp + "%");
-  console.log(temp);
+    var w_width = document.body.clientWidth;
+    var b_padding = $(".mid_block").css("padding-left"); // padding转数字
+    var b_margin = $(".mid_block").css("margin-left"); // margin转数字
+    if (b_padding) // 主页/tag页
+    {
+        b_padding = (Number(b_padding.replace("px", "")));
+        b_margin = (Number(b_margin.replace("px", "")));
+        var b_offset = b_padding + b_margin;
+
+        var rate = cal_rate(w_width);
+        console.log(rate);
+
+        if (w_width < 1200) { // 半页
+          var b_width = w_width * rate;
+          var b_left = (w_width - b_width) / 2;
+          $(".mid_block").css("width", b_width + "px");
+          $(".mid_block").css("left", b_left - b_offset + "px"); // 减去多余的padding
+        } else {
+          ;
+        }
+    }
+}
+
+function cal_rate (width)
+{
+  if (width < 800)
+  {
+    return 1 - 0.000125 * width;
+  }
+  else if (width < 1200)
+  {
+    return 1.5 - 0.00075 * width;
+  }
+  else
+  {
+    return 0.56 - 0.00014 * width;
+  }
 }
 
 function getScrollTop() // 计算滚动条
