9db6e400a12cd74f0d6a6b783c67034dabfb677b
Lynx <1197225628@qq.com>
2019-05-05 19:09:12 +0800

    section定位

--- source/js/guide.js
+++ source/js/guide.js
@@ -1,7 +1,7 @@
 // 全局变量
 var h_index = 0;
 // 结构体
-var h_strut = {
+var h_struct = {
   rank: 0,// 1 for h1, 2 for h2 ...
   name: "" // innerText
 };
@@ -12,15 +12,32 @@ function create_guide() {
 
   var left_block = document.getElementsByClassName("left_block")[0];
   dfs(document.body);
+    
+  var left_guide = document.createElement("div");
+  left_guide.setAttribute("class", "left_guide");
   for (var i = 0; i < h_index; i ++)
   {
     console.log(my_guide[i].rank, my_guide[i].name);
+    var temp_a = document.createElement("a");
+    temp_a.id = "guide_" + i;
+    temp_a.innerText = my_guide[i].name;
+    temp_a.innerHTML += "<br>";
+    left_guide.appendChild(temp_a);
   }
   
-  var left_guide = document.createElement("div");
-  left_guide.setAttribute("class", "left_guide");
-
   left_block.appendChild(left_guide);
+  for (var i = 0; i < h_index; i ++)
+  {
+    document.querySelector("#guide_" + i).setAttribute("onclick", "add_scroll(" + i + ")");
+  }
+}
+
+function add_scroll (i)// 注意i的值
+{
+  document.querySelector("#section_" + i).scrollIntoView({
+    block: "start",
+    behavior: "smooth"
+  });
 }
 
 // 深度优先搜索
@@ -29,21 +46,20 @@ function dfs(my_node) {
   if (my_node.tagName == 'H1')
   {
     temp = 1;
-    h_index ++;
-  }
+    }
   else if (my_node.tagName == 'H2')
   {
     temp = 2;
-    h_index ++;
-  }
+    }
   else if (my_node.tagName == 'H3')
   {
     temp = 3;
-    h_index ++;
-  }
+    }
   if (temp != 0)
   {
-    var t_struct = Object.create(h_strut);
+    my_node.innerHTML = "<div id='section_" + h_index + "'>" + my_node.innerHTML + "</div>";// 给文章中的section增加id
+    h_index ++;
+    var t_struct = Object.create(h_struct);
     t_struct.rank = temp;
     t_struct.name = my_node.innerText;
     my_guide.push(t_struct);// 注意数组传参的方式
