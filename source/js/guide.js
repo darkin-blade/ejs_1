f9fc9b190a44c9781767585b4f468966c85a1179
Lynx <1197225628@qq.com>
2019-05-05 17:19:18 +0800

    修改标题样式

--- source/js/guide.js
+++ source/js/guide.js
@@ -1,1 +1,21 @@
-console.log('guide.js')
+function dfs(my_node){
+  if (my_node.tagName == 'H1')
+  {
+    console.log(my_node.innerHTML);
+  }
+  else if (my_node.tagName == 'H2')
+  {
+    console.log(my_node.innerHTML);    
+  }
+  else if (my_node.tagName == 'H3')
+  {
+    console.log(my_node.innerHTML);    
+  }
+  var my_child = my_node.childNodes.length;
+  for (var i = 0; i < my_child; i ++)
+  {
+    dfs(my_node.childNodes[i]);
+  }
+}
+
+dfs(document.body);
