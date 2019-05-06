66ccb79c6fc7ff6428ea7479c2d440f9496916a5
Lynx <1197225628@qq.com>
2019-05-06 20:21:06 +0800

    code颜色

--- source/js/highlight.js
+++ source/js/highlight.js
@@ -2,28 +2,58 @@
  * 手写html代码块高亮
  */
 
-function dfs_c(my_node) {
+function dfs_c(my_node, found) {
   if (my_node.className != null)
   {
     if (my_node.className.match("highlight") != null)
     {
+      found = 1;
       var temp_type = my_node.className.replace(/highlight /, "");
-      console.log(temp_type);
+      // console.log(temp_type);
       my_highlight(my_node, temp_type);
+    } // else
+    if ((found)&&(my_node.className == "string"))// 字符串
+    {
+      my_node.className += " code_string";
+    }
+    if ((found)&&(my_node.className == "comment"))// 注释
+    {
+      my_node.className += " code_comment";
+    }
+    if ((found)&&(my_node.className == "keyword"))// 关键字
+    {
+      my_node.className += " code_keyword";
+    }
+    if ((found)&&(my_node.className == "attribute"))// 属性
+    {
+      my_node.className += " code_attribute";
+    }
+    if ((found)&&(my_node.className == "attr"))// 属性(html)
+    {
+      my_node.className += " code_attr";
+    }
+    if ((found)&&(my_node.className == "built_in"))// ???
+    {
+      my_node.className += " code_built_in";
+    }
+    if ((found)&&(my_node.className == "tag"))// html标签
+    {
+      my_node.className += " code_tag";
     }
   }
 
   var my_child = my_node.childNodes.length;
   for (var i = 0; i < my_child; i ++)
   {
-    dfs_c(my_node.childNodes[i]);
+    dfs_c(my_node.childNodes[i], found);
   }
 }
 
 function my_highlight(my_node, type) {
   my_node.className = my_node.className + " my_code";
+  // console.log(my_node.innerText.replace(/\n/, ""));
 }
 
 function my_highlight_start() {
-  dfs_c(document.body);
+  dfs_c(document.body, 0);
 }
