70ce950cf1da3e5c5bcc942beebce67283f21647
Lynx <2104934038@qq.com>
2019-09-01 10:04:39 +0800

    date margin-bottom改成负数

--- source/js/change.js
+++ source/js/change.js
@@ -134,7 +134,7 @@ function change_img()
           break;
         }
         var temp_w = $(temp_father).width();
-        var temp_h = $(temp_father).height() - 100;
+        var temp_h = $(temp_father).height() - 50;
         var ratio_w = temp_w / my_img.width;
         var ratio_h = temp_h / my_img.height;
         var son_ratio = my_img.width / my_img.height;// 图片自身的宽高比
