64c301dfb04cb6a6d9cc917e6b769a0ec741ed8a
lynx <2104934038@qq.com>
2019-11-18 14:58:26 +0800

    更正了sb写的搜索代码

--- source/js/search.js
+++ source/js/search.js
@@ -4,11 +4,12 @@
 
 var searchFunc = function (path, search_id, content_id) 
 {
-  // var BTN = "<i id='local-search-close'>x</i>";
+  var each_length = 200;// 每篇文章显示的长度,可能靠后的关键词显示不了
+  var head_length = 10;// 每篇文章第一个关键字之前的长度
   $.ajax({
     url: path,
     dataType: "xml",
-    success: function (xmlResponse) {
+    success: function (xmlResponse) {// 获取search.xml(默认),在内部进行查找
       var datas = $("entry", xmlResponse).map(function () {
         return {
           title: $("title", this).text(),
@@ -23,67 +24,64 @@ var searchFunc = function (path, search_id, content_id)
       $input.addEventListener('input', function () {
         // var str = '<ul class=\"search-result-list\">';
         var str = '<ul>';
-        var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
-        $resultContent.innerHTML = "";
-        if (this.value.trim().length <= 0) {
+        var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);// 搜索的关键字(去除头尾空格),用`+`分割
+        $resultContent.innerHTML = "";// 清空之前结果
+        if (this.value.trim().length <= 0) {// 如果没有输入关键字
           return;
         }
-        datas.forEach(function (data) {
+        datas.forEach(function (data, index_data) {// 对每一篇文章进行匹配
           var isMatch = true;
-          var content_index = [];
-          if (!data.title || data.title.trim() === '') {
+          if (!data.title || data.title.trim() === '') {// 文章没有标题
             data.title = "Untitled";
           }
           var data_title = data.title.trim().toLowerCase();
-          var data_content = data.content.trim().replace(/<[^>]+>/g, "").toLowerCase();
+          var data_content = data.content.trim().replace(/<[^>]+>/g, "").toLowerCase();// 文章内容
+          if (index_data == 0) {
+            console.log("文章内容: " + data_content);
+          }
           var data_url = data.url;
           var index_title = -1;
           var index_content = -1;
           var first_occur = -1;
-          if (data_content !== '') {
-            keywords.forEach(function (keyword, i) {
-              index_title = data_title.indexOf(keyword);
+          if (data_content !== '') {// 文章内容非空
+            keywords.forEach(function (keyword, i) {// 对每个关键字查找第一次出现的位置, (当前元素, 索引, 正在操作的数组)
+              index_title = data_title.indexOf(keyword);// 搜索关键字第一次出现的位置
               index_content = data_content.indexOf(keyword);
 
-              if (index_title < 0 && index_content < 0) {
+              if (index_title < 0 && index_content < 0) {// 关键字没有出现过
                 isMatch = false;
               } else {
                 if (index_content < 0) {
                   index_content = 0;
                 }
-                if (i == 0) {
-                  first_occur = index_content;
+                if (i == 0) {// 如果是第一次出现
+                  first_occur = index_content;// 记录第一次出现的位置
                 }
               }
             });
-          } else {
+          } else {// 文章为空
             isMatch = false;
           }
           if (isMatch) {
-            // str += "<li><a href='" + unescape(decodeURI(data_url)) + "' class='search-result-title'>" + data_title + "</a>";
+            // 显示文章标题
             str += "<li><a href='" + unescape(decodeURI(data_url)) + "'>" + data_title + "</a>";
             var content = data.content.trim().replace(/<[^>]+>/g, "");
             if (first_occur >= 0) {
-              var start = first_occur - 20;
-              var end = first_occur + 80;
+              var start = first_occur - head_length;
+              var end = each_length;
 
-              if (start < 0) {
+              if (start < 0) {// 如果关键字在文章很前面
                 start = 0;
               }
 
-              if (start == 0) {
-                end = 100;
-              }
-
-              if (end > content.length) {
-                end = content.length;
+              if (end + each_length > content.length) {// 不能超过文章长度
+                end = content.length - each_length;
               }
 
               var match_content = content.substr(start, end);
 
-              keywords.forEach(function (keyword) {
+              keywords.forEach(function (keyword) {// 高亮关键字
                 var regS = new RegExp(keyword, "gi");
-                // match_content = match_content.replace(regS, "<em class=\"search-keyword\">" + keyword + "</em>");
                 match_content = match_content.replace(regS, "<strong class='search-highlight'>" + keyword + "</strong>");
               });
 
@@ -94,18 +92,10 @@ var searchFunc = function (path, search_id, content_id)
         });
         str += "</ul>";
         if (str.indexOf('<li>') === -1) {
-          // return $resultContent.innerHTML = BTN + "<ul><span class='local-search-empty'>没有找到内容，更换下搜索词试试吧~<span></ul>";
-          // 不想用关闭功能
           return $resultContent.innerHTML = "<ul><span>Not found<span></ul>";
         }
-        // $resultContent.innerHTML = BTN + str;
-        // 不想用关闭功能
         $resultContent.innerHTML = str;
       });
     }
-  });
-  $(document).on('click', '#local-search-close', function () { // 关闭
-    $('#local-search-input').val('');
-    $('#local-search-result').html('');
   });
 }
